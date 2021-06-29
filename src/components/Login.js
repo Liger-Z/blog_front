3b26e0d393793821fdaafd3af918f2d95cf6277b
Liger <jtadub@gmail.com>
2021-06-29 15:25:42 +1200

    Add foundation for login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,50 @@
+import { useState } from 'react';
+
 const Login = () => {
-return (
-  <div className="div-container">
-  </div>
-)
-}
+  const [field, setField] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    const target = event.target;
+
+    setField({
+      ...field,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label>Username</label>
+        <br />
+        <input
+          type="text"
+          name="username"
+          value={field.username}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          value={field.password}
+          onChange={handleChange}
+        />
+        <br/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
